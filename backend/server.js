0deb6f24abd05c0c5e7877d5088727b89e74da47
saidali2 <sacidka212@gmail.com>
2025-06-07 15:12:47 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,8 @@ app.post('/search', (req, res) => {
   const fakeResults = [
     { name: 'Smartphone X', price: '$199', source: 'Jumia' },
     { name: 'Shoes Pro', price: '$49', source: 'Kilimall' },
+    { name: 'TV Box Pro', price: '$69', source: 'Alibaba' },
+    { name: 'Magic Pillow', price: '$12', source: 'UnknownShop' }
   ];
 
   const results = fakeResults.filter(item =>
